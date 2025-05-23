66705be338df68302dbc92ff3ef612dc2d3d00e0
5h9igzqanx <5h9igzqanx@users.noreply.github.com>
2025-05-23 12:39:28 +0000

    deploy: cb21fc4cf397e23d05e42d34529fc3c2bbaa4008

--- js/lvJing.min.3308360d461f8a6309e378973d622aaf75847d64f6a353f510e6136edd759aa4.js
+++
@@ -1,1 +0,0 @@
-var date=new Date,today=String(date.getMonth()+1)+","+String(date.getDate()),ZHONGDASHIJIANJINIANRI=["09,18","12,13"];ZHONGDASHIJIANJINIANRI.includes(today)&&document.querySelectorAll("*").forEach(function(e){e.style.filter="grayscale(1)"}),console.log("正常工作。缅怀先烈，珍惜和平。")
