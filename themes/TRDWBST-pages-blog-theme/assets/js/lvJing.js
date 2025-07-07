da82ab65ee798ab2aab25f5f89551438f81efa50
5h9igzqanx <116637326+5h9igzqanx@users.noreply.github.com>
2025-07-07 16:30:02 +0800

    Update lvJing.js

--- themes/TRDWBST-pages-blog-theme/assets/js/lvJing.js
+++ themes/TRDWBST-pages-blog-theme/assets/js/lvJing.js
@@ -1,6 +1,6 @@
 var date = new Date();
 var today=String(date.getMonth()+1)+","+String(date.getDate())
-var ZHONGDASHIJIANJINIANRI=["09,18","12,13"]
+var ZHONGDASHIJIANJINIANRI=["07,07","09,18","12,13"]
 if(ZHONGDASHIJIANJINIANRI.includes(today)){
     document.querySelectorAll("*").forEach(function(ele){
         ele.style.filter='grayscale(1)'
