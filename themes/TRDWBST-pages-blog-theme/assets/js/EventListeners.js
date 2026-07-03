15cb8d05c35535e87e03969e5883dc9d8e468395
5h9igzqanx <116637326+5h9igzqanx@users.noreply.github.com>
2026-07-03 17:49:06 +0800

    Update EventListeners.js

--- themes/TRDWBST-pages-blog-theme/assets/js/EventListeners.js
+++ themes/TRDWBST-pages-blog-theme/assets/js/EventListeners.js
@@ -1,13 +1,15 @@
-document.addEventListener("visibilitychange",function(){
-    var _originTitle=document.title
-    let _time
-    if(document.hidden){
-        document.title="你干甚去啊 | "+_originTitle
-        clearTimeout(_time)
-    }else{
-        document.title="欢迎回来 | "+_originTitle
-        _time=setTimeout(function(){
-            document.title=_originTitle
-        },5000)
-    }
-})
+function autoChangeTabTitieWhileLeaving(){
+    document.addEventListener("visibilitychange",function(){
+        var _originTitle=document.title
+        let _time
+        if(document.hidden){
+            document.title="你干甚去啊 | "+_originTitle
+            clearTimeout(_time)
+        }else{
+            document.title="欢迎回来 | "+_originTitle
+            _time=setTimeout(function(){
+                document.title=_originTitle
+            },5000)
+        }
+    })
+}
