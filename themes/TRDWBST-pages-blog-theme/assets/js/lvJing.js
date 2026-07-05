dcc24118638bc0cab85ac8eda2660256533eff7f
5h9igzqanx <116637326+5h9igzqanx@users.noreply.github.com>
2026-07-05 16:32:08 +0800

    master

--- themes/TRDWBST-pages-blog-theme/assets/js/lvJing.js
+++
@@ -1,9 +0,0 @@
-var date = new Date();
-var today=String(date.getMonth()+1)+","+String(date.getDate())
-var ZHONGDASHIJIANJINIANRI=["07,07","09,18","12,13"]
-if(ZHONGDASHIJIANJINIANRI.includes(today)){
-    document.querySelectorAll("*").forEach(function(ele){
-        ele.style.filter='grayscale(1)'
-    })
-}
-// 缅怀先烈
